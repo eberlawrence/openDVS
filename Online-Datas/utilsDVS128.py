2444922a624f307349a5aaf13c6263c598e7bcfc
Eber Lawrence <eberlawrence@ufu.br>
2019-07-15 17:27:12 -0300

    Updating

--- Online-Datas/utilsDVS128.py
+++ Online-Datas/utilsDVS128.py
@@ -3,7 +3,44 @@ import math
 from scipy import signal
 import pygame
 
-def boundingBoxPart(screen, x, y, M):
+class BoundingBox(object):
+    
+    def __init__(self, screen, x, y, M):
+        self.x = x
+        self.y = y
+        self.screen = screen
+        self.coord = np.array(list(zip(x, y)))
+        self.partic = []
+        self.M = M
+    
+    def checkNeighborhood(self, pos, l=3):        
+        return pos[(-l <= pos[:,0]) & (pos[:,0] <= l) & (-l <= pos[:,1]) & (pos[:,1] <= l)]
+        
+    def createParticles(self):
+        xy = np.array(list(zip(self.x, self.y)))
+        while len(xy) > 0:            
+            auxXY = xy - xy[0]
+            auxXY = self.checkNeighborhood(auxXY) + xy[0]
+            self.partic.append(auxXY)
+            
+            break
+            var1 = list(map(tuple, xy))        
+            var2 = list(map(tuple, auxXY))           
+            xy = np.array(list(set(var1) - set(var2)))       
+    
+    def plotParticles(self):
+        self.createParticles()
+        print("\n", self.partic[0])   
+        for p in self.partic:            
+            if len(p) > 3:
+                Px = int(np.mean(np.array(p[:,0])))
+                Py = int(np.mean(np.array(p[:,1])))
+                pygame.draw.circle(self.screen, (255, 0, 0), (Px * self.M, Py * self.M), self.M * 10, 2)
+            
+            
+    
+
+def boundingBoxPart(screen, x, y, M):      
     matrix = np.zeros([128, 128])
     for i in range(len(x)):
         matrix[x[i], y[i]] = 1
