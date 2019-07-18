fb3a1a9bb7e06baec167fe9e4db1741770e78102
Eber Lawrence <eberlawrence@ufu.br>
2019-07-17 21:31:08 -0300

    Updating particle tracking

--- Online-Datas/real-time-DVS128.py
+++ Online-Datas/real-time-DVS128.py
@@ -4,49 +4,24 @@ from time import time
 import numpy as np
 import utilsDVS128
 
-
 HOST = ''
 PORT = 8000
 clock = pygame.time.Clock()
-m = 7
-
-class DisplayDVS128:
-    pygame.display.set_caption('Neuromorphic Camera - DVS128')
-
-    def __init__(self, width, height):
-        self.width = width * m
-        self.height = height * m
-        self.background = (128, 128, 128)
-        self.colorPos = (255, 255, 255)
-        self.colorNeg = (0, 0, 0)
-        self.gameDisplay = pygame.display.set_mode((self.width, self.height))
-        self.gameDisplay.fill(self.background)
-
-    def plotEvents(self, pol, x, y, ts):
-        i = 0
-        while i < len(pol):
-            if pol[i] == 0:
-                self.gameDisplay.fill(self.colorPos, ((127 - x[i]) * m, y[i] * m, m, m))
-                i += 1
-            else:
-                self.gameDisplay.fill(self.colorNeg, ((127 - x[i]) * m, y[i] * m, m, m))
-                i += 1
-	
-    def printFPS(self, fps):
-        font = pygame.font.SysFont('Segoe UI', 20)
-        txtFPS = font.render('FPS: {0:.2f}'.format(fps), True, (0, 0, 0))
-        self.gameDisplay.blit(txtFPS,(0,0))
-		
 
 def main():
     t = time()
+    stop = False
     pygame.init()
-    displayEvents = DisplayDVS128(128, 128)
+    displayEvents = utilsDVS128.DisplayDVS128(128, 128)
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp.bind((HOST, PORT))
     pol, x, y, ts = [], [], [], []
 
-    while True:
+    while not stop:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                stop = True
+                
         vet = []
         msg, cliente = udp.recvfrom(10000)
 
@@ -58,12 +33,12 @@ def main():
         x.extend(vet[size : 2 * size])
         y.extend(vet[2 * size : 3 * size])
         ts.extend(vet[3 * size : ])
-        if np.sum(ts) >= 5000: # 6 fps
+        if np.sum(ts) >= 10000: # 6 fps
             
             displayEvents.gameDisplay.fill(displayEvents.background)
             displayEvents.plotEvents(pol, x, y, ts)
-            bB = utilsDVS128.BoundingBox(displayEvents.gameDisplay, x, y, m)
-            bB.plotParticles()
+            bB = utilsDVS128.BoundingBox(displayEvents.gameDisplay, x, y)
+            bB.particlesFromFrames()
             t2 = time() - t               
             displayEvents.printFPS(1/t2)            
             pygame.display.update()
@@ -72,7 +47,6 @@ def main():
        
     pygame.quit()
     udp.close()
-
 
 if __name__ == "__main__":
 	main()
