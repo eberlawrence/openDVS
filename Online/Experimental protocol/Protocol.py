98bb044b64f51975a1a20811db79c74c65b9ffc4
Eber Lawrence <eberlawrence@hotmail.com>
2021-03-25 13:17:22 -0300

    Updating

--- Online/Experimental protocol/Protocol.py
+++ Online/Experimental protocol/Protocol.py
@@ -1,9 +1,8 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 import random
 
 subject_info = pd.read_csv('subject_info.csv', sep='\t', encoding='utf-8', index_col=0)
-subject_task_info = pd.DataFrame(columns=['Preensão_Esperada', 'Preensão_Classificada', 'Angulação_Esperada', 'Angulação_Obtida', 'Tempo_Gasto', 'Objeto_Escorregou'])
 
 
 input("\n\nPressione ENTER para iniciar.\n\n")
@@ -15,21 +14,20 @@ name = input("\nNome: ")
 age = input("\nIdade: ")
 gender = input("\nSexo: ")
 
-
 subject_info = subject_info.append({'Nome': name, 'Idade': age, 'Sexo': gender}, ignore_index=True)
 
 subject_info.to_csv("subject_info.csv", sep='\t', encoding='utf-8')
 print(subject_info)
 
 
-object_list_one = ["Caneta", "Copo", "Colher", "Garrafa", "Chave", "Caixa", "Faca", "Celular", 
-                   "Barbeador", "Lata de bebida", "Chave de fenda", "Bola", "Escova de dentes", 
+object_list_one = ["Caneta", "Copo", "Colher", "Garrafa", "Chave", "Caixa", "Faca", "Celular",
+                   "Barbeador", "Lata de bebida", "Chave de fenda", "Bola", "Escova de dentes",
                    "Pote", "Parafuso", "Carteira", "Isqueiro", "Pêra", "Chave Allen", "Laranja"]
 object_list_one_tripod = object_list_one[::2]
 object_list_one_power = object_list_one[1::2]
 
 
-object_list_two = ["Chave fixa", "Estojo de óculos", "Lapiseira", "Lâmpada", 
+object_list_two = ["Chave fixa", "Estojo de óculos", "Lapiseira", "Lâmpada",
                    "Pen drive", "Calculadora", "Pincel", "HD", "Pilha", "Câmera"]
 object_list_two_tripod = object_list_two[::2]
 object_list_two_power = object_list_two[1::2]
@@ -38,6 +36,20 @@ input("\n\nIniciar a primeira etapa do procedimento.\n\n")
 
 blocks = 2
 objetcts_per_block = 4
+
+block_list, sample_list = [], []
+for i in range(blocks):
+    for j in range(objetcts_per_block):
+        block_list.append("BLOCO" + str(i + 1))
+        sample_list.append(j)
+
+
+columns = ['Preensão_Esperada', 'Preensão_Classificada', 'Angulação_Esperada', 'Angulação_Obtida', 'Tempo_Gasto', 'Objeto_Escorregou']
+index = [block_list, sample_list]
+
+subject_task_info_one = pd.DataFrame(columns=columns, index=index)
+
+a = pd.DataFrame(columns=["A", "B", "C", "D"], index=[np.array(["BLOCO1", "BLOCO1", "BLOCO2"]), np.array([1,2,1])])
 
 for i in range(blocks):
     print("\nLista de objetos 1 - Bloco " + str(i + 1) + " de " + str(blocks) + ".\n\n")
@@ -51,27 +63,22 @@ for i in range(blocks):
         angle = np.random.randint(-90,90)
         print("\nObjeto almejado: ", ordered_list[j])
         print("\nÂngulo do objeto: ", angle)
-        input("\n\nPressione ENTER para continuar.\n")  
-        grasp_classified = input("\nPreensão classificada: ")  
+        input("\n\nPressione ENTER para continuar.\n")
+        grasp_classified = input("\nPreensão classificada: ")
         angle_obtained = input("\nÂngulo obtido: ")
         time_spent = input("\nTempo gasto: ")
         object_slip = input("\nEscorregamento do objeto: ")
+        subject_task_info_one.loc[('BLOCO' + str(i + 1), j), :] = {'Preensão_Esperada': "Power" if object_list_one.index(ordered_list[j]) % 2 else "Tripod",
+                                                      'Preensão_Classificada': grasp_classified,
+                                                      'Angulação_Esperada': angle,
+                                                      'Angulação_Obtida': angle_obtained,
+                                                      'Tempo_Gasto': time_spent,
+                                                      'Objeto_Escorregou': object_slip}
 
-        subject_task_info = subject_task_info.append({'Preensão_Esperada': "Power" if object_list_one.index(ordered_list[j]) % 2 else "Tripod", 
-                                                      'Preensão_Classificada': grasp_classified, 
-                                                      'Angulação_Esperada': angle, 
-                                                      'Angulação_Obtida': angle_obtained, 
-                                                      'Tempo_Gasto': time_spent, 
-                                                      'Objeto_Escorregou': object_slip}, 
-                                                      ignore_index=True)
-        input("\nPressione ENTER para continuar.\n\n")  
-    print(subject_task_info)
-    print("\n\nBloco " + str(i + 1) + " finalizado!\n\n")  
-    input("\nPressione ENTER para continuar.\n\n")  
-
-subject_task_info.to_csv("subject_task_info.csv", sep='\t', encoding='utf-8')
+        input("\nPressione ENTER para continuar.\n\n")
+    print(subject_task_info_one)
+    print("\n\nBloco " + str(i + 1) + " finalizado!\n\n")
+    input("\nPressione ENTER para continuar.\n\n")
 
 
-# a = subject_task_info.iloc[:4,:]
-# b = subject_task_info.iloc[4:,:]
-# c = pd.concat([ a, b ], keys={ 'Bloco 1' : a, 'Bloco 2' : b })
+subject_task_info_one.to_csv(str(name) + "_task_info_one.csv", sep='\t', encoding='utf-8')
--- Online/arduinoCodes/setOrientation/setOrientation/setOrientation.ino
+++ Online/arduinoCodes/setOrientation/setOrientation/setOrientation.ino
@@ -1,38 +1,80 @@
 
-#define time_per_revolution 20000000 // 20 seconds
-#define steps_per_revolution 3200
+#define time_per_revolution 2000000 // 2 seconds
+#define steps_per_revolution 3200   // 3200 steps
+#define angle_per_step 0.1125       // 0.1125 degrees
+
 
 int PUL = 3; //define Pulse pin
 int DIR = 2; //define Direction pin
 int ENA = 5; //define Enable Pin
-bool flag = true;
+bool start_flag = true;
+
+float time_per_step = time_per_revolution / steps_per_revolution;
 
 void setup() {
   pinMode (PUL, OUTPUT);
   pinMode (DIR, OUTPUT);
   pinMode (ENA, OUTPUT);
+  Serial.begin(115200);
 }
 
-void loop() {
 
-  if (Serial.available()) {
-    input = Serial.read();
-    Serial.print("Digite o ângulo de -90 até +90: " );
-    Serial.println(input);
+int angle = 0;
+int sign = 1;
+int steps = 0;
+
+void loop() {
+  if (start_flag == true) {
+    Serial.println("Digite o ângulo de -90 até +90:" );
+    Serial.print("\n");
+    start_flag = false;
   }
-  //  if (flag == true) {
-  //    delay(500);
-  //    float time_per_step = time_per_revolution / steps_per_revolution;
-  //
-  //    for (int i = 0; i < steps_per_revolution; i++)
-  //    {
-  //      digitalWrite(DIR, LOW);
-  //      digitalWrite(ENA, HIGH);
-  //      digitalWrite(PUL, HIGH);
-  //      delayMicroseconds(time_per_step / 2);
-  //      digitalWrite(PUL, LOW);
-  //      delayMicroseconds(time_per_step / 2);
-  //    }
-  //    flag = false;
-  //  }
+
+
+  if ( Serial.available())
+  {
+    char ch = Serial.read();
+    if (ch >= '0' && ch <= '9') {
+      angle = (angle * 10) + (ch - '0');
+    }
+    else if ( ch == '-') {
+      sign = -1;
+    }
+    else  {
+      angle = angle * sign ;
+      Serial.print("Ângulo: ");
+      Serial.print(angle);
+      Serial.print(" graus\n\n\n");
+
+
+      steps = round(abs(angle) / angle_per_step);
+
+      if (angle > 0) {
+        for (int i = 0; i < steps; i++)
+        {
+          digitalWrite(DIR, LOW);
+          digitalWrite(ENA, HIGH);
+          digitalWrite(PUL, HIGH);
+          delayMicroseconds(time_per_step / 2);
+          digitalWrite(PUL, LOW);
+          delayMicroseconds(time_per_step / 2);
+        }
+      }
+      else if (angle < 0) {
+        for (int i = 0; i < steps; i++)
+        {
+          digitalWrite(DIR, HIGH);
+          digitalWrite(ENA, HIGH);
+          digitalWrite(PUL, HIGH);
+          delayMicroseconds(time_per_step / 2);
+          digitalWrite(PUL, LOW);
+          delayMicroseconds(time_per_step / 2);
+        }
+      }
+
+      angle = 0;
+      sign = 1;
+      start_flag = true;
+    }
+  }
 }
